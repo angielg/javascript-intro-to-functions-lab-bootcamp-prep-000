6ba70de374395b01300507f212fec21cb406642d
Angie Goza <angielg9888@gmail.com>
2018-05-19 21:36:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(string) {
   console.log('hello')
 }
 function sayHiToGrandma(string) {
-  console.log("hello")
+  console.log("string.toLowerCase")
   return "I can't hear you!"
 }
